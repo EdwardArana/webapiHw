0cac7d7f99f9263f89d6c20293e391244af5099d
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-16 20:25:22 -0800

    working on my questions on js

---
+++ javascript.js
@@ -0,0 +1,42 @@
+var testQuestions = [
+    {
+        question1: "Who has a song called 'Like You' ",
+        answers: {
+
+            a: "Chingy",
+            b: "Bow Wow",
+            c: "Lil Romeo",
+            d: "Ja Rule",
+        },
+        correctAnswer: b,
+
+        question2: "Who is the lowest rating artist?",
+        answers: {
+
+            a: "Iggy Azalea",
+            b: "Shakira",
+            c: "Beyonce",
+            d: "Ariana Grande",
+        },
+        correctAnswer: a,
+
+        question3: "Who sang the song Jump?",
+        answers: {
+            a: "Lil Romeo",
+            b: "Kris Kross",
+            c: "Drake",
+            d: "Will Thorne",
+        },
+        correctAnswer: b,
+
+        question4: "Who sang the popular song 'It's Hip To Be Square'",
+        answers: {
+            a: "Huey Lewis and The News",
+            b: "Tom Swayer",
+            c: "Tom Petty",
+            d: "Rush",
+        },
+        correctAnswer: c,
+        
+    }
+];
